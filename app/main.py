d0662fedafdda014f5807b26f9d8c0ac9efebe0e
greyhoundforty <ryantiffany@fastmail.com>
2024-11-22 03:06:28 -0600

    Adding in updated pylint versions

--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
-from fastapi import FastAPI
+"""
+Simple fastapi to test tailscale operator and funnel
+"""
+
 import platform
 import os
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/hello")
 def hello():
+    """
+    Return a simple hello message 
+    """
     return {"message": "Hello, World!"}
 
 @app.get("/status")
 def status():
+    """
+    Return container and host information
+    """
     return {
         "container_id": os.getenv("HOSTNAME"),
         "python_version": platform.python_version(),
         "system": platform.system(),
         "platform": platform.platform()
     }
-
